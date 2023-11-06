5a480274cdb4a16bc2fa7a5394bc9183848d8269
Neaz-mq <neazmorshed666@gmail.com>
2023-11-06 20:02:15 +0600

    added see more button in All services

--- src/components/AllServices/AllServices.jsx
+++ src/components/AllServices/AllServices.jsx
@@ -3,31 +3,47 @@ import Tour from "../Tour/Tour";
 
 
 const AllServices = () => {
-    const [tours, setTours] = useState([]);
-    // this is not the best way to load show all data
-    const [dataLength, setDataLength] = useState(6);
-  
-    
-    useEffect(() => {
-      fetch('http://localhost:5000/tours')
-        .then((res) => res.json())
-        .then((data) => setTours(data));
-    }, []);
+  const [isShow, setIsShow] = useState(false);
+  const [tours, setTours] = useState([]);
 
-    return (
-        <div>
-        <div className="text-center">
-          <h2 className="text-5xl font-bold p-14 mb-5" data-aos="zoom-in">All Services</h2>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-1 md:ml-52 lg:grid-cols-2 gap-10  lg:mx-8 mx-5 my-12">
-          {tours.slice(0, dataLength).map((tour) => (
-            <Tour  key={tour.id} tour={tour}></Tour>
-        
-          ))}
-        </div>
-        
+
+
+  useEffect(() => {
+    fetch('http://localhost:5000/tours')
+      .then((res) => res.json())
+      .then((data) => setTours(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="text-center">
+        <h2 className="text-5xl font-bold p-14 mb-5" data-aos="zoom-in">All Services</h2>
       </div>
-    );
+      <div className="grid grid-cols-1 md:grid-cols-1 md:ml-52 lg:grid-cols-1 gap-16  lg:mx-8 mx-5 my-12">
+
+
+        {
+          isShow ? tours.map((tour) => (
+
+
+            <Tour key={tour.id} tour={tour}></Tour>
+          ))
+
+            : tours.slice(0, 4).map((tour) => (
+              <Tour key={tour.id} tour={tour}></Tour>
+            ))
+        }
+
+
+      </div>
+      <div className="text-center pb-5">
+        {tours.length > 4 && <button onClick={() => setIsShow(!isShow)} className="btn btn-secondary">
+          {isShow ? "See less" : "See More"}
+        </button>}
+      </div>
+
+    </div>
+  );
 };
 
 export default AllServices;
