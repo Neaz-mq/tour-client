c6a7269f385c0079f1681a831b5d8649a9a3e571
Neaz-mq <neazmorshed666@gmail.com>
2023-11-08 10:20:02 +0600

    added react-simple-typewriter package

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-
+import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
   return (
@@ -6,7 +6,19 @@ const Banner = () => {
       <div className="hero-overlay bg-opacity-75"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-lg" data-aos="fade-down">
-          <h1 className="mb-5 lg:text-5xl lg:mr-0 lg:ml-0 text-2xl font-bold text-white my-8">Let's Visit The World</h1>
+          <h1 className="mb-5 lg:text-5xl lg:mr-0 lg:ml-0 text-2xl font-bold text-white my-8">Let's  <span style={{ color: 'red', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Visit The World!']}
+            loop={15}
+            cursor
+            cursorStyle=''
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </span></h1>
           <p className="mb-5  lg:mr-0 lg:ml-0 lg:text-2xl text-base font-medium ">Book unique and memorable travel experience guided by locals. </p>
           <button className="btn btn-error text-white">Book Now</button>
         </div>
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,4 +1,4 @@
-
+import { Typewriter } from 'react-simple-typewriter'
 
 const Details = () => {
     return (
@@ -8,7 +8,19 @@ const Details = () => {
   <div className="hero-content flex-col lg:flex-row-reverse">
     <img src="https://i.ibb.co/bQ2zXqP/pexels-malte-luk-2432299.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Local Tour and Guide!</h1>
+      <h1 className="text-5xl font-bold">Local <span style={{ color: 'red', fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Tour and Guide!']}
+            loop={15}
+            cursor
+            cursorStyle=''
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </span></h1>
       <p className="py-6 text-lg font-medium">A tour guide  is a person who provides assistance, and information on cultural, historical and contemporary heritage </p>
       <button className="btn btn-primary">Read More</button>
     </div>
