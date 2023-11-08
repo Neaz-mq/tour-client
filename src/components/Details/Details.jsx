bf1cd98c0a40f926c0b824df1d425dce100cbb21
Neaz-mq <neazmorshed666@gmail.com>
2023-11-08 11:29:29 +0600

    lottiefiles package used

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,8 +5,8 @@ const Details = () => {
         <div className="hero min-h-screen bg-base-200" data-aos="fade-up"
         data-aos-easing="ease-out-cubic"
         data-aos-duration="2000">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://i.ibb.co/bQ2zXqP/pexels-malte-luk-2432299.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+  <div className="hero-content flex-col lg:flex-row-reverse ">
+  <iframe src="https://lottie.host/?file=c6e1870b-1883-4ebc-869b-2be9acfc11f5/n1JF3NNln4.json" className='w-1/2 h-96'></iframe>
     <div>
       <h1 className="text-5xl font-bold">Local <span style={{ color: 'red', fontWeight: 'bold' }}>
           {/* Style will be inherited from the parent element */}
